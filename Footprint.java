527feb8b50d025ba9406f9a60b1173bde730033a
kalu06 <crivelli.lucas@gmail.com>
2013-03-21 14:47:50 -0300

    Create Footprint.java

---
+++ Footprint.java
@@ -0,0 +1,12 @@
+public class Footprint implements Parser {
+
+  @Override
+	private ArrayList<LinkedList<Hashtable<Integer, String>>> Parse() {
+		
+	}
+	
+	@Override
+	private boolean Validate(DTO object){
+		
+	}
+}
