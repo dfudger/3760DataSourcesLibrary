911fc793dab74f2ba9649e56a0b8b289546220ba
kalu06 <crivelli.lucas@gmail.com>
2013-03-21 14:50:31 -0300

    Update Footprint.java

--- Footprint.java
+++ Footprint.java
@@ -1,12 +1,7 @@
 public class Footprint implements Parser {
-
   @Override
-	private ArrayList<LinkedList<Hashtable<Integer, Object>>> Parse() {
-		
-	}
+  private ArrayList<LinkedList<Hashtable<Integer, Object>>> Parse() {}
 	
-	@Override
-	private boolean Validate(String file){
-		
-	}
+  @Override
+  private boolean Validate(String file){}
 }
