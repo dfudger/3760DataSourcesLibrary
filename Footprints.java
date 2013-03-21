82b6ded261a834608cc4c2b6297acc0e60f9e81f
kalu06 <crivelli.lucas@gmail.com>
2013-03-21 14:52:47 -0300

    Update and rename Footprint.java to Footprints.java

--- Footprints.java
+++ Footprints.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Footprint implements Parser {
+public class Footprints implements Parser {
   @Override
   private ArrayList<Hashtable<Integer, Object>> Parse() {}
 	
