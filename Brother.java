3fd302d0b1651c13d542c0095747f37e472f51b6
Matt Stark <mstark@uoguelph.ca>
2013-03-27 22:23:19 -0400

    Basic shell for the Brother class for Big Brother Parser Implementation

---
+++ Brother.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashTable;
+import java.io.File;
+
+public class Brother implements Parser {
+	public ArrayList<Hashtable<Integer, Object>> parse() {
+		/* Open or access internet.xml */
+		/* parse it for data*/
+		/* store the info found in the database tags (as a string) into the Hashtable as an object*/
+		/* return the ArrayList with the data inside*/
+	}
+
+	public boolean validate(String xml) {
+		
+	}
+
+	public String getFieldFile() {
+		return "BigBrotherFields.txt";
+	}
+}
